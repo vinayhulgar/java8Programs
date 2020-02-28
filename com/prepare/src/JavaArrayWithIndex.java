85fd948cf45abfc00d8077d2fd7e36a4d855b4cb
vinay <vinay.hulgar@gmail.com>
2020-02-28 15:17:09 +0530

    Add files via upload

---
+++ com/prepare/src/JavaArrayWithIndex.java
@@ -0,0 +1,24 @@
+package com.model.src;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/*
+
+To Print Array or List with Index in front.
+
+*/
+public class JavaArrayWithIndex {
+
+	public static void main(String[] args) {
+		String [] names = {"Java", "Node", "JavaScript","Rust", "Go"};
+		
+		List<String> collect = IntStream.range(0, names.length)
+				.mapToObj(index -> index+ ":" +names[index])
+				.collect(Collectors.toList());
+		collect.forEach(System.out::println);
+
+	}
+
+}
