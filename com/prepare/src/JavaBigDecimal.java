0ccf71a47f51f34a9053b5dd111a3d3cabaa0705
vinay <vinay.hulgar@gmail.com>
2020-03-02 18:01:18 +0530

    Add files via upload

---
+++ com/prepare/src/JavaBigDecimal.java
@@ -0,0 +1,22 @@
+package com.model.src;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaBigDecimal {
+
+	public static void main(String[] args) {
+		List<BigDecimal> invoices = new LinkedList<>();
+		invoices.add(BigDecimal.valueOf(9.9));
+		invoices.add(BigDecimal.valueOf(1.0));
+		invoices.add(BigDecimal.valueOf(19.99));
+		invoices.add(BigDecimal.valueOf(0.2));
+		invoices.add(BigDecimal.valueOf(5.5));
+		
+		BigDecimal sum = invoices.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+		System.out.println("Sum: "+sum);
+		
+	}
+
+}
